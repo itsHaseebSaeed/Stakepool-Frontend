2e3de0a33b222f4bf97e6f201e92c08d5fb9ef63
Haseeb30000 <34556583+Haseeb30000@users.noreply.github.com>
2021-09-21 18:51:07 +0500

    second commit

--- assets/page_content.1693f72e.js
+++
@@ -1,1 +0,0 @@
-import{r,o as e,c as o,b as s}from"./vendor.5a76559d.js";const t={},a={class:"page-content"};t.render=function(t,n){const c=r("router-view");return e(),o("div",a,[s(c)])};export{t as default};
